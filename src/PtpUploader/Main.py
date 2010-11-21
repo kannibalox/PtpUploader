8f354ceb365026bc7a1264dbf21c23bfc2e412c5
TnS-hun <TnS-hun@users.noreply.github.com>
2010-11-21 17:43:40 +0000

    [added] Release description maker now can create torrent and can start it seeding.

--- src/PtpUploader/Main.py
+++ src/PtpUploader/Main.py
@@ -9,20 +9,30 @@ import sys;
 def Initialize():
 	Settings.LoadSettings()
 	Globals.InitializeGlobals( Settings.WorkingPath )
-	Globals.Logger.info( "PtpUploader v0.1 by TnS" )
+	Globals.Logger.info( "PtpUploader v0.2 by TnS" )
+	Globals.Logger.info( "Usage:" )
+	Globals.Logger.info( "Main.py without parameterss starts the bot listening for announcements." )
+	Globals.Logger.info( "\"Main.py <target directory or filename>\" creates the release description and starts seeding the torrent." )
+	Globals.Logger.info( "\"Main.py --notorrent <target directory or filename>\" creates the release description." )
+	Globals.Logger.info( "" )
 
 def MainBotMode():
 	Ptp.Login()
 	ptpUploader = PtpUploader()
 	ptpUploader.Work()
 
-def MainReleaseInfoMode(path):
-	ReleaseInfoMaker.MakeReleaseInfo( path )
+def MainReleaseInfoMode():
+	if len( sys.argv ) == 2:
+		releaseInfoMaker = ReleaseInfoMaker( sys.argv[ 1 ] )
+		releaseInfoMaker.MakeReleaseInfo( createTorrent = True )
+	elif len( sys.argv ) == 3 and sys.argv[ 1 ] == "--notorrent":
+		releaseInfoMaker = ReleaseInfoMaker( sys.argv[ 2 ] )
+		releaseInfoMaker.MakeReleaseInfo( createTorrent = False )
 
 if __name__ == '__main__':
 	Initialize()
-	
-	if len( sys.argv ) == 2: 
-		MainReleaseInfoMode( sys.argv[ 1 ] )
+
+	if len( sys.argv ) > 1:
+		MainReleaseInfoMode()
 	else:
 		MainBotMode()
--- src/PtpUploader/ReleaseInfoMaker.py
+++ src/PtpUploader/ReleaseInfoMaker.py
@@ -1,40 +1,92 @@
 from Source.Manual import Manual
+
 from AnnouncementWatcher import *
+from MakeTorrent import MakeTorrent
 from MediaInfo import MediaInfo
 from ReleaseInfo import ReleaseInfo
+from Rtorrent import Rtorrent
 from ScreenshotMaker import ScreenshotMaker
+from Settings import Settings
 
 import os
 
 class ReleaseInfoMaker:
-	@staticmethod
-	def MakeReleaseInfo(path):
-		# TODO: add support for directories
-		if not os.path.isfile( path ):
-			print "File '%s' doesn't exists!" % path
-			return
+	def __init__(self, path):
+		self.Path = path
+		self.ReleaseName = None
+		self.WorkingDirectory = None
+		self.TorrentDataPath = None
+		self.VideoFiles = []
 
-		releasePath = os.path.dirname( path )
-		screenshotPath = os.path.join( releasePath, "screenshot.png" );
+	def CollectVideoFiles(self):
+		if os.path.isdir( self.Path ):
+			# Make sure that path doesn't ends with a trailing slash or else os.path.split would return with wrong values.
+			self.Path = self.Path.rstrip( "\\/" )
+
+			# If path is a directory we search for video files.
+			files = os.listdir( self.Path );
+			for file in files:
+				filePath = os.path.join( self.Path, file );
+				if os.path.isfile( filePath ) and Settings.HasValidVideoExtensionToUpload( filePath ):
+					self.VideoFiles.append( filePath );
+
+			if len( self.VideoFiles ) <= 0:
+				print "Path '%s' doesn't contains any videos!" % self.Path
+				return False
+
+			# We use the parent directory of the path as the working directory.
+			# Release name will be the directory's name. Eg. it will be "anything" for "/something/anything"
+			self.WorkingDirectory, self.ReleaseName = os.path.split( self.Path )
+			self.TorrentDataPath = self.Path
+		elif os.path.isfile( self.Path ):
+			self.VideoFiles.append( self.Path )
+			
+			# We use same the directory where the file is as the working directory.
+			# Release name will be the file's name.
+			self.WorkingDirectory, self.ReleaseName = os.path.split( self.Path )
+			self.TorrentDataPath = self.WorkingDirectory
+		else:
+			print "Path '%s' doesn't exists!" % self.Path
+			return False
+
+		return True
+
+	def MakeReleaseInfo(self, createTorrent):
+		if not self.CollectVideoFiles():
+			return
+		
+		# Make sure the files we are generating are not present.
+
+		screenshotPath = os.path.join( self.WorkingDirectory, "screenshot.png" )
 		if os.path.exists( screenshotPath ):
 			print "Can't create screenshot because '%s' already exists!" % screenshotPath
 			return
 		
-		releaseDescriptionFilePath = os.path.join( releasePath, "release description.txt" );
+		releaseDescriptionFilePath = os.path.join( self.WorkingDirectory, "release description.txt" )
 		if os.path.exists( releaseDescriptionFilePath ):
 			print "Can't create release description because '%s' already exists!" % releaseDescriptionFilePath
 			return
 
-		videoFiles = [ path ]
+		torrentName = "PTP " + self.ReleaseName + ".torrent";
+		torrentPath = os.path.join( self.WorkingDirectory, torrentName );
+		if createTorrent and os.path.exists( torrentPath ):
+			print "Can't create torrent because '%s' already exists!" % uploadTorrentPath
+			return
 
 		# Get the media info.
-		mediaInfos = MediaInfo.ReadAndParseMediaInfos( videoFiles );
+		mediaInfos = MediaInfo.ReadAndParseMediaInfos( self.VideoFiles )
 
 		# Take and upload screenshots.
-		uploadedScreenshots = ScreenshotMaker.TakeAndUploadScreenshots( videoFiles[ 0 ], screenshotPath, mediaInfos[ 0 ].DurationInSec );
+		uploadedScreenshots = ScreenshotMaker.TakeAndUploadScreenshots( self.VideoFiles[ 0 ], screenshotPath, mediaInfos[ 0 ].DurationInSec )
 
 		# Make the release description.
 		manualSource = Manual()
-		announcement = Announcement( announcementFilePath = "", source = manualSource, id = "", releaseName = path )
-		releaseInfo = ReleaseInfo( announcement, imdbId = "" ) 
-		releaseInfo.PtpUploadInfo.FormatReleaseDescription( releaseInfo, uploadedScreenshots, mediaInfos, releaseDescriptionFilePath );
+		announcement = Announcement( announcementFilePath = "", source = manualSource, id = "", self.ReleaseName )
+		releaseInfo = ReleaseInfo( announcement, imdbId = "" )
+		releaseInfo.PtpUploadInfo.FormatReleaseDescription( releaseInfo, uploadedScreenshots, mediaInfos, releaseDescriptionFilePath )
+
+		# Create the torrent
+		if createTorrent:
+			MakeTorrent.Make( self.Path, torrentPath )
+			rtorrent = Rtorrent()
+			rtorrent.AddTorrentSkipHashCheck( torrentPath, self.TorrentDataPath )
