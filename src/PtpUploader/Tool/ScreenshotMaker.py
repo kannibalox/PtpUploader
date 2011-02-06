194c1c8214ba51e19733816dc2980ecf897b2eb3
TnS-hun <TnS-hun@users.noreply.github.com>
2011-02-06 17:58:40 +0000

    [changed] Sorting is done using the size and the name of the video to make sure that we take media info and screens from the movie and not from the extras.

--- src/PtpUploader/Tool/ScreenshotMaker.py
+++ src/PtpUploader/Tool/ScreenshotMaker.py
@@ -64,20 +64,44 @@ class ScreenshotMaker:
 		if errorCode != 0:
 			raise PtpUploaderException( "Process execution '%s' returned with error code '%s'." % ( args, errorCode ) )			
 
-	# Sorting is needed to ensure that the screenshot is taken from the first video when a release contains multiple videos.
-	# We don't want to post screenshots that contains spoilers. :)
+	# We sort video files by their size (less than 50 MB difference is ignored) and by their name.
+	# Sorting by name is needed to ensure that the screenshot is taken from the first video to avoid spoilers when a release contains multiple videos.
+	# Sorting by size is needed to ensure that we don't take the screenshots from the sample or extras included.
+	# Ignoring less than 50 MB differnece is needed to make sure that CD1 will be sorted before CD2 even if CD2 is larger than CD1 by 49 MB.   
 	@staticmethod
 	def SortVideoFiles(files):
+		class SortItem:
+			def __init__(self, path):
+				self.Path = path
+				self.LowerPath = path.lower()
+				self.Size = os.path.getsize( path )
+
+			@staticmethod
+			def Compare( item1, item2 ):
+				sizeDifference = item1.Size - item2.Size 
+				if abs( sizeDifference ) > ( 50 * 1024 * 1024 ):
+					if item1.Size > item2.Size:
+						return -1
+					else:
+						return 1
+
+				if item1.LowerPath < item2.LowerPath:
+					return -1
+				elif item1.LowerPath > item2.LowerPath:
+					return 1
+				else:
+					return 0
+
 		filesToSort = []
 		for file in files:
-			item = file.lower(), file # Add as a tuple.
+			item = SortItem( file )
 			filesToSort.append( item )
 
+		filesToSort.sort( cmp = SortItem.Compare )
+
 		files = []
-		filesToSort.sort()
 		for item in filesToSort:
-			path = item[ 1 ] # First element is the path in lower case, second is the original path.
-			files.append( path )
+			files.append( item.Path )
 
 		return files
 
