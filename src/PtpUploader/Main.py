4410cb795154f1654f41f1d5a045dd190f36a071
TnS-hun <TnS-hun@users.noreply.github.com>
2011-03-20 22:09:17 +0000

    [changed] Moved MainWeb into Main

--- src/PtpUploader/Main.py
+++ src/PtpUploader/Main.py
@@ -1,17 +1,35 @@
+from WebServer.MyWebServer import MyWebServer
+
 from MyGlobals import MyGlobals
-from Ptp import Ptp
 from PtpUploader import PtpUploader
 from Settings import Settings
+from Database import InitDb
 
 def Initialize():
 	Settings.LoadSettings()
 	MyGlobals.InitializeGlobals( Settings.WorkingPath )
 
-def MainBotMode():
-	Ptp.Login()
-	ptpUploader = PtpUploader()
-	ptpUploader.Work()
+def Run():
+	print "Starting..."
+	InitDb()
+
+	MyGlobals.PtpUploader = PtpUploader()
+
+	# Do not start the web server if the username or the password is not set.
+	webServerThread = None
+	if len( Settings.WebServerUsername ) > 0 and len( Settings.WebServerPassword ) > 0:
+		webServerThread = MyWebServer()
+		webServerThread.start()
+
+	try:
+		MyGlobals.PtpUploader.Work()
+	except (KeyboardInterrupt, SystemExit):
+		pass
+
+	print "Stopping..."
+	if webServerThread is not None:
+		webServerThread.StopServer()
 
 if __name__ == '__main__':
 	Initialize()
-	MainBotMode()
+	Run()
