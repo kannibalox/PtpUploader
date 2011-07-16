48d074d7060dbec196b63aafac7d12fdd7ceec3c
TnS-hun <TnS-hun@users.noreply.github.com>
2011-07-16 21:48:45 +0000

    [added] TorrentLeech IRC support.

--- src/pyWHATauto/PtpUploaderIrcBot.py
+++ src/pyWHATauto/PtpUploaderIrcBot.py
@@ -1,23 +1,41 @@
 # This is just a wrapper for pyWA by JohnnyFive.
 # pyWA does all the hard work for us hanging on IRC. We just handle the downloads. :)
 
-import WHATauto;
+import WHATauto
 
-import os;
-import re;
-import sys;
+import os
+import re
+import sys
+import threading
 
-WorkingDirectoryPath = "";
-AnnouncementDirectoryPath = "";
+WorkingDirectoryPath = ""
+AnnouncementDirectoryPath = ""
+
+class AnnouncementFileWriter:
+	def __init__(self, path):
+		self.Path = path
+		
+	def Write(self):
+		file = open( self.Path, "w" )
+		file.close()
 
 def SendAnnouncementToPtpUploader(source, id, releaseName):
-	global AnnouncementDirectoryPath;
+	global AnnouncementDirectoryPath
 	
 	# Create the announcement file for PtpUploader.
-	fileName = "[source=%s][id=%s][title=%s]" % ( source, id, releaseName );
-	announcementFilePath = os.path.join( AnnouncementDirectoryPath, fileName );
-	file = open( announcementFilePath, "w" );
-	file.close();
+	fileName = "[source=%s][id=%s][title=%s]" % ( source, id, releaseName )
+	announcementFilePath = os.path.join( AnnouncementDirectoryPath, fileName )
+	announcementFileWriter = AnnouncementFileWriter( announcementFilePath )
+
+	# TODO: make a config for delaying instead of this quick hack.
+	#if releaseName.lower().find( "720p" ) == -1:
+	announcementFileWriter.Write()
+	#else:
+	#	print "Delaying release '%s' by one hour." % releaseName
+	#	timer = threading.Timer( 90 * 60, announcementFileWriter.Write )
+	#	timer.start()
+
+# TODO: the announcement parsing in pyWHATauto already parser the regex, would be nice to use that instead of these.
 
 def HandleGftAutoAnnouncement(announcement):
 	match = re.match( r"NEW :: ([^:]+) :: ([^:]+) :: http://www\.thegft\.org/details\.php\?id=([\d]+) :: .*", announcement );
@@ -26,6 +44,14 @@ def HandleGftAutoAnnouncement(announcement):
 		return;
 
 	SendAnnouncementToPtpUploader( "gft", id = match.group( 3 ), releaseName = match.group( 1 ) );
+
+def HandleTlAutoAnnouncement(announcement):
+	match = re.match( r"New Torrent Announcement: <([^>]*)>[\W]*Name:'([^']*)[\W]*uploaded by '([^']*)' -\W+http://www.torrentleech.org/torrent/(\d+)", announcement );
+	if match is None:
+		print "PtpUploaderIrcBot can't parse TL announcement: '%s'." % announcement;
+		return;
+
+	SendAnnouncementToPtpUploader( "tl", id = match.group( 4 ), releaseName = match.group( 2 ) );
 
 def MyDownload(downloadID, downloadType, site, location=False, network=False, target=False, retries=0, email=False, notify=False, filterName=False, announce=False, formLogin=False, sizeLimits=False, name=False):
 	global AnnouncementDirectoryPath;
@@ -36,6 +62,8 @@ def MyDownload(downloadID, downloadType, site, location=False, network=False, ta
 	if announce: # Automatic announcements come with the full announcement message.
 		if site == "thegft":
 			HandleGftAutoAnnouncement( announce );
+		elif site == "torrentleech":
+			HandleTlAutoAnnouncement( announce );
 
 def main(argv):
 	global AnnouncementDirectoryPath;
