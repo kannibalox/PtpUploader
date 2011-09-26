e9db8ca6471610122fca643a972d231523f4665c
TnS-hun <TnS-hun@users.noreply.github.com>
2011-09-26 21:15:00 +0000

    [fixed] In some cases ffmpeg writes SAR instead of PAR. Now using DAR.

--- src/PtpUploader/Tool/Ffmpeg.py
+++ src/PtpUploader/Tool/Ffmpeg.py
@@ -21,31 +21,33 @@ class Ffmpeg:
 		proc.wait()
 		result = stderr.decode( "utf-8", "ignore" )
 
-		# Formatting can be one of the following:		
+		# Formatting can be one of the following. PAR can be SAR too.
 		# Stream #0.0(eng): Video: h264, yuv420p, 1280x544, PAR 1:1 DAR 40:17, 24 tbr, 1k tbn, 48 tbc
 		# Stream #0.0[0x1e0]: Video: mpeg2video, yuv420p, 720x480 [PAR 8:9 DAR 4:3], 7500 kb/s, 29.97 tbr, 90k tbn, 59.94 tbc
-		match = re.search( r"(\d+)x(\d+), PAR (\d+):(\d+) DAR", result )
+		match = re.search( r"(\d+)x(\d+), [SP]AR \d+:\d+ DAR (\d+):(\d+)", result )
 		if match is None:
-			match = re.search( r"(\d+)x(\d+) \[PAR (\d+):(\d+) DAR", result )
+			match = re.search( r"(\d+)x(\d+) \[[SP]AR \d+:\d+ DAR (\d+):(\d+)", result )
 		if match is None:
 			return
 			
 		width = int( match.group( 1 ) )
 		height = int( match.group( 2 ) )
-		parX = int( match.group( 3 ) )
-		parY = int( match.group( 4 ) )
+		darX = int( match.group( 3 ) )
+		darY = int( match.group( 4 ) )
 		# We ignore invalid resolutions, invalid aspect ratios and aspect ratio 1:1.			
-		if width <= 0 or height <= 0 or parX <= 0 or parY <= 0 or ( parX == 1 and parY == 1 ):
+		if width <= 0 or height <= 0 or darX <= 0 or darY <= 0 or ( darX == 1 and darY == 1 ):
 			return
 		
-		width = ( width * parX ) / parY
-		width = int( width )
+		newWidth = ( height * darX ) / darY
+		newWidth = int( newWidth )
+		if abs( newWidth - width ) <= 1:
+			return
 
 		# For FFmpeg frame size must be a multiple of 2.
-		if ( width % 2 ) != 0:
-			width += 1
+		if ( newWidth % 2 ) != 0:
+			newWidth += 1
 
-		self.ScaleSize = "%sx%s" % ( width, height )
+		self.ScaleSize = "%sx%s" % ( newWidth, height )
 
 	def MakeScreenshotInPng(self, timeInSeconds, outputPngPath):
 		self.Logger.info( "Making screenshot with ffmpeg from '%s' to '%s'." % ( self.InputVideoPath, outputPngPath ) )
