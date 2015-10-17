9f7fb0ba4f0368bd82c067c4498e28cb1b1f85a7
TnS-hun <TnS-hun@users.noreply.github.com>
2015-10-17 18:27:58 +0200

    [changed] Updated the comment.

--- src/PtpUploader/Tool/Rtorrent.py
+++ src/PtpUploader/Tool/Rtorrent.py
@@ -17,7 +17,7 @@ import xmlrpclib
 class Rtorrent:
 	def __init__(self):
 		MyGlobals.Logger.info( "Initializing PyroScope." )
-				
+
 		load_config.ConfigLoader().load()
 		self.proxy = config.engine.open()
 
@@ -29,11 +29,11 @@ class Rtorrent:
 		file = open( torrentPath, "rb" );
 		contents = xmlrpclib.Binary( file.read() );
 		file.close();
-		
+
 		torrentData = bencode.bread( torrentPath ); 
 		metafile.check_meta( torrentData );
 		infoHash = metafile.info_hash( torrentData );
-		
+
 		self.proxy.load_raw( contents );
 
 		# If load_raw is slow then set_directory_base throws an exception (Fault: <Fault -501: 'Could not find info-hash.'>),
@@ -47,7 +47,7 @@ class Rtorrent:
 			except Exception:
 				if maximumTries > 1:
 					maximumTries -= 1
-					time.sleep( 2 ) # Six seconds.
+					time.sleep( 2 ) # Two seconds.
 				else:
 					raise
 
@@ -59,7 +59,7 @@ class Rtorrent:
 	# Returns with the info hash of the torrent.
 	def AddTorrentSkipHashCheck(self, logger, torrentPath, downloadPath):
 		logger.info( "Adding torrent '%s' without hash checking to rTorrent to '%s'." % ( torrentPath, downloadPath ) );		
-		
+
 		sourceDirectory, sourceFilename = os.path.split( torrentPath );
 		sourceFilename = "fast resume " + sourceFilename;
 		destinationTorrentPath = os.path.join( sourceDirectory, sourceFilename );
@@ -68,7 +68,7 @@ class Rtorrent:
 			raise PtpUploaderException( "Can't create fast resume torrent because path '%s' already exists." % destinationTorrentPath )
 		
 		shutil.copyfile( torrentPath, destinationTorrentPath );
-		
+
 		metainfo = bencode.bread( destinationTorrentPath )
 		metafile.add_fast_resume( metainfo, downloadPath.encode( 'utf-8' ) )
 		bencode.bwrite( destinationTorrentPath, metainfo )
@@ -82,7 +82,7 @@ class Rtorrent:
 			os.remove( destinationTorrentPath )
 
 		return infoHash
-		
+
 	def IsTorrentFinished(self, logger, infoHash):
 		# TODO: this try catch block is here because xmlrpclib throws an exception when it timeouts or when the torrent with the given info hash doesn't exists.
 		# The latter error most likely will cause stuck downloads so we should add some logic here to cancel an upload. For example: if it haven't download a single byte in ten minutes we can cancel it.
@@ -96,7 +96,7 @@ class Rtorrent:
 			logger.exception( "Got exception while trying to check torrent's completion status. Info hash: '%s'." % infoHash );
 
 		return False
-	
+
 	# It doesn't delete the data.
 	def DeleteTorrent(self, logger, infoHash):
 		try:
@@ -108,7 +108,7 @@ class Rtorrent:
 	# rTorrent can't download torrents with fast resume information in them, so we clean the torrents before starting the download.
 	# This can happen if the uploader uploaded the wrong torrent to the tracker.
 	def CleanTorrentFile(self, logger, torrentPath):
-		logger.info( "Cleaning torrent file '%s'." % torrentPath )		
+		logger.info( "Cleaning torrent file '%s'." % torrentPath )
 
 		metainfo = bencode.bread( torrentPath )
 		metafile.clean_meta( metainfo, including_info = False, logger = logger.info )
