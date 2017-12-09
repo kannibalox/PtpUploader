069b564389d56518ba65c725569673733d4878fb
kannibalox <kannibalox@gmail.com>
2017-12-09 23:14:27 +0000

    CG improvements

--- src/PtpUploader/Source/Cinemageddon.py
+++ src/PtpUploader/Source/Cinemageddon.py
@@ -4,10 +4,7 @@ from Source.SourceBase import SourceBase
 
 from Helper import DecodeHtmlEntities, GetSizeFromText, RemoveDisallowedCharactersFromPath, ValidateTorrentFile
 from MyGlobals import MyGlobals
-from NfoParser import NfoParser
 from PtpUploaderException import PtpUploaderException
-from ReleaseExtractor import ReleaseExtractor;
-from ReleaseInfo import ReleaseInfo;
 
 import os
 import re
@@ -18,7 +15,7 @@ class Cinemageddon(SourceBase):
 
 		self.Name = "cg"
 		self.NameInSettings = "Cinemageddon"
-	
+
 	def IsEnabled(self):
 		return len( self.Username ) > 0 and len( self.Password ) > 0
 
@@ -39,15 +36,15 @@ class Cinemageddon(SourceBase):
 		descriptionEndIndex = html.find( '<p><a name="startcomments"></a></p>' )
 		if descriptionEndIndex == -1:
 			raise PtpUploaderException( JobRunningState.Ignored_MissingInfo, "Description can't found on torrent page. Probably the layout of the site has changed." )
-		
-		description = html[ :descriptionEndIndex ]			
+
+		description = html[ :descriptionEndIndex ]
 
 		# We will use the torrent's name as release name.
 		if not parseForExternalCreateJob:
 			matches = re.search( r'href="download.php\?id=(\d+)&name=.+">(.+)\.torrent</a>', description )
 			if matches is None:
 				raise PtpUploaderException( JobRunningState.Ignored_MissingInfo, "Can't get release name from torrent page." )
-		
+
 			releaseInfo.ReleaseName = DecodeHtmlEntities( matches.group( 2 ) )
 
 		# Get source and format type
@@ -62,9 +59,15 @@ class Cinemageddon(SourceBase):
 
 			if matches is None:
 				raise PtpUploaderException( JobRunningState.Ignored_MissingInfo, "Can't get release source and format type from torrent page." )
-			
+
 			sourceType = matches.group( 3 )
 			formatType = matches.group( 4 )
+
+			if '/' in sourceType and not releaseInfo.ResolutionType:
+				sourceType, _, resolutionType = sourceType.partition( '/' )
+				resolutionType.strip( 'p' )
+				if resolutionType in ["720", "1080"]:
+					releaseInfo.ResolutionType = resolutionType
 
 		# Get IMDb id.
 		if ( not releaseInfo.HasImdbId() ) and ( not releaseInfo.HasPtpId() ):
@@ -89,7 +92,7 @@ class Cinemageddon(SourceBase):
 		if description.find( '>Type</td><td valign="top" align=left>XXX<' ) != -1:
 			raise PtpUploaderException( JobRunningState.Ignored_Forbidden, "Marked as XXX." )
 
-		self.__MapSourceAndFormatToPtp( releaseInfo, sourceType, formatType )
+		self.__MapSourceAndFormatToPtp( releaseInfo, sourceType, formatType, html )
 
 		# Make sure that this is not a wrongly categorized DVDR.
 		if ( not releaseInfo.IsDvdImage() ) and ( re.search( r"\.vob</td>", description, re.IGNORECASE ) or re.search( r"\.iso</td>", description, re.IGNORECASE ) ):
@@ -106,26 +109,21 @@ class Cinemageddon(SourceBase):
 
 		self.__ParsePage( logger, releaseInfo, response )
 
-	def __MapSourceAndFormatToPtp(self, releaseInfo, sourceType, formatType):
+	def __MapSourceAndFormatToPtp(self, releaseInfo, sourceType, formatType, html):
 		sourceType = sourceType.lower()
 		formatType = formatType.lower()
 
-		# Adding BDrip support would be problematic because there is no easy way to decide if it is HD or SD.
-		# Maybe we could use the resolution and file size. But what about the oversized and upscaled releases? 
-		
-		if releaseInfo.IsResolutionTypeSet():
-			releaseInfo.Logger.info( "Resolution type '%s' is already set, not getting from the torrent page." % releaseInfo.ResolutionType )
-		else:
-			releaseInfo.ResolutionType = "Other"
 
 		if releaseInfo.IsSourceSet():
 			releaseInfo.Logger.info( "Source '%s' is already set, not getting from the torrent page." % releaseInfo.Source )
-		elif sourceType == "dvdrip":
+		elif sourceType in [ "dvdrip", "dvd-r" ]:
 			releaseInfo.Source = "DVD"
 		elif sourceType == "vhsrip":
 			releaseInfo.Source = "VHS"
 		elif sourceType == "tvrip":
 			releaseInfo.Source = "TV"
+		elif sourceType in [ "webrip", "web-dl" ]:
+			releaseInfo.Source = "WEB"
 		else:
 			raise PtpUploaderException( JobRunningState.Ignored_NotSupported, "Unsupported source type '%s'." % sourceType )
 
@@ -137,16 +135,44 @@ class Cinemageddon(SourceBase):
 			releaseInfo.Codec = "XviD"
 		elif formatType == "divx":
 			releaseInfo.Codec = "DivX"
+		elif formatType == "dvd-r":
+			if releaseInfo.Size > 4700000000:
+				releaseInfo.Codec = "DVD9"
+			else:
+				releaseInfo.Codec = "DVD5"
 		else:
 			raise PtpUploaderException( JobRunningState.Ignored_NotSupported, "Unsupported format type '%s'." % formatType )
-	
+
+		# Adding BDrip support would be problematic because there is no easy way to decide if it is HD or SD.
+		# Maybe we could use the resolution and file size. But what about the oversized and upscaled releases?
+
+		if releaseInfo.IsResolutionTypeSet():
+			releaseInfo.Logger.info( "Resolution type '%s' is already set, not getting from the torrent page." % releaseInfo.ResolutionType )
+		elif releaseInfo.IsDvdImage():
+			if re.search( r'Standard +: NTSC', html ):
+				releaseInfo.ResolutionType = 'NTSC'
+			elif re.search( r'Standard +: PAL', html ):
+				releaseInfo.ResolutionType = 'PAL'
+			else:
+				releaseInfo.Logger.info( "DVD detected but could not find resolution" )
+		else:
+			releaseInfo.ResolutionType = "Other"
+
+		if releaseInfo.IsDvdImage() and not releaseInfo.Container:
+			if re.search( r"\.vob</td>", html, re.IGNORECASE ):
+				releaseInfo.Container = "VOB IFO"
+			elif re.search( r"\.iso</td>", html, re.IGNORECASE ):
+				releaseInfo.Container = "ISO"
+			else:
+				releaseInfo.Logger.info( "DVD detected but could not find container" )
+
 	def PrepareDownload(self, logger, releaseInfo):
 		if releaseInfo.IsUserCreatedJob():
 			self.__DownloadNfo( logger, releaseInfo )
 		else:
 			# TODO: add filtering support for Cinemageddon
 			# In case of automatic announcement we have to check the release name if it is valid.
-			# We know the release name from the announcement, so we can filter it without downloading anything (yet) from the source. 
+			# We know the release name from the announcement, so we can filter it without downloading anything (yet) from the source.
 			#if not ReleaseFilter.IsValidReleaseName( releaseInfo.ReleaseName ):
 			#	logger.info( "Ignoring release '%s' because of its name." % releaseInfo.ReleaseName )
 			#	return None
@@ -168,9 +194,8 @@ class Cinemageddon(SourceBase):
 		if response.find( "<h2>Max Torrents Reached</h2>" ) != -1:
 			raise PtpUploaderException( "Maximum torrents reached on CG." )
 
-		file = open( path, "wb" )
-		file.write( response )
-		file.close()
+		with open( path, "wb" ) as fh:
+			fh.write( response )
 
 		ValidateTorrentFile( path )
 
@@ -179,8 +204,8 @@ class Cinemageddon(SourceBase):
 	def GetCustomUploadPath(self, logger, releaseInfo):
 		# TODO: if the user forced a release name, then let it upload by that name.
 		if releaseInfo.IsZeroImdbId():
-			raise PtpUploaderException( "Uploading to CG with zero IMDb ID is not yet supported." % text ) 		
-		
+			raise PtpUploaderException( "Uploading to CG with zero IMDb ID is not yet supported." % text )
+
 		# If the movie already exists on PTP then the IMDb info is not populated in ReleaseInfo.
 		if len( releaseInfo.InternationalTitle ) <= 0 or len( releaseInfo.Year ) <= 0:
 			imdbInfo = Imdb.GetInfo( logger, releaseInfo.GetImdbId() )
@@ -190,16 +215,16 @@ class Cinemageddon(SourceBase):
 				releaseInfo.Year = imdbInfo.Year
 
 		if len( releaseInfo.InternationalTitle ) <= 0:
-			raise PtpUploaderException( "Can't rename release because international title is empty." )			
+			raise PtpUploaderException( "Can't rename release because international title is empty." )
 
 		if len( releaseInfo.Year ) <= 0:
-			raise PtpUploaderException( "Can't rename release because year is empty." )			
+			raise PtpUploaderException( "Can't rename release because year is empty." )
 
 		name = "%s (%s) %s %s" % ( releaseInfo.InternationalTitle, releaseInfo.Year, releaseInfo.Source, releaseInfo.Codec )
 		name = RemoveDisallowedCharactersFromPath( name )
 
 		logger.info( "Upload directory will be named '%s' instead of '%s'." % ( name, releaseInfo.ReleaseName ) )
-		
+
 		newUploadPath = releaseInfo.GetReleaseUploadPath()
 		newUploadPath = os.path.dirname( newUploadPath )
 		newUploadPath = os.path.join( newUploadPath, name )
@@ -213,7 +238,7 @@ class Cinemageddon(SourceBase):
 		if result is None:
 			return ""
 		else:
-			return result.group( 1 )	
+			return result.group( 1 )
 
 	def GetUrlFromId(self, id):
 		return "http://cinemageddon.net/details.php?id=" + id
