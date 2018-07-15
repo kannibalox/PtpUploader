61d5601753a6b6f4ee0a24c0b74580c351b39fd9
kannibalox <kannibalox@gmail.com>
2018-07-15 18:53:30 +0000

    Use imdbpy for iMDB info

--- src/PtpUploader/InformationSource/Imdb.py
+++ src/PtpUploader/InformationSource/Imdb.py
@@ -2,16 +2,12 @@ from MyGlobals import MyGlobals
 from PtpUploaderException import PtpUploaderException
 
 import simplejson as json
+from imdb import IMDb
 
 import hashlib
 import hmac
 import time
 import uuid
-
-# Uses the undocumented iPhone IMDb API.
-# See:
-# - imdbmobile: http://code.google.com/p/imdbmobile/
-# - IMDb-Python-API: https://github.com/dbr/IMDb-Python-API
 
 class ImdbInfo:
 	def __init__(self):
@@ -26,50 +22,20 @@ class ImdbInfo:
 class Imdb:
 	@staticmethod
 	def __GetInfoInternal(imdbId):
-		# timestamp is needed
-		# device is needed but can be anything
-		# appid is needed but can be anything
-		# sig must be "and2" and it must be the last parameter
-		timeStamp = int( time.time() )
-		device = uuid.uuid1()
-		url = "http://app.imdb.com/title/maindetails?tconst=tt%s&timestamp=%s&device=%s&locale=en_US&appid=android2&sig=and2" % ( imdbId, timeStamp, device )
-
-		# Sign the URL.
-		# "eRnAYqbvj2JWXyPcu62yCA" comes from the disassembled "IMDb Movies & TV" Android application by IMDb. 
-		url += "-%s" % hmac.HMAC( "eRnAYqbvj2JWXyPcu62yCA", url, hashlib.sha1 ).hexdigest()
-
-		result = MyGlobals.session.get( url )
-		result.raise_for_status()
-		jsonLoad = result.json()
-
+		ia = IMDb()
+		movie = ia.get_movie( imdbId )
 		imdbInfo = ImdbInfo()
-		data = jsonLoad[ "data" ]
-		imdbInfo.Title = data[ "title" ].strip()
-		imdbInfo.Year = data[ "year" ]
-		imdbInfo.ImdbRating = str( data.get( "rating", "" ) )
-		imdbInfo.ImdbVoteCount = str( data.get( "num_votes", "" ) )
 
-		image = data.get( "image" )
-		if image:
-			imdbInfo.PosterUrl = image[ "url" ]
-
-			# Some posters are huge on IMDb but can be easily scaled to arbitrary size by changing the URL.			
-			height = image.get( "height" )
-			if ( height is not None ) and height > 768:
-				imdbInfo.PosterUrl = imdbInfo.PosterUrl.replace( "._V1_.jpg", "._V1_SY768_.jpg" )
-			
-		plot = data.get( "plot" )
-		if plot:
-			imdbInfo.Plot = plot[ "outline" ]
-
-		seasons = data.get( "seasons" )
-		if seasons:
+		imdbInfo.Title = movie[ "title" ].strip()
+		imdbInfo.Year = movie[ "year" ]
+		imdbInfo.ImdbRating = str( movie.get( "rating", "" ) )
+		imdbInfo.ImdbVoteCount = str( movie.get( "num_votes", "" ) )
+		if 'cover' in movie:
+			imdbInfo.PosterUrl = re.sub( r"\._V1_.*\.jpg", "._V1_SY768_.jpg", movie[ "cover" ] )
+		if 'episode of' in movie:
 			imdbInfo.IsSeries = True
-
-		series = data.get( "series" )
-		if series:
-			imdbInfo.IsSeries = True
-
+		if 'plot outline' in movie:
+			imdbInfo.Plot = movie[ "plot outline" ]
 		return imdbInfo
 		
 	@staticmethod
