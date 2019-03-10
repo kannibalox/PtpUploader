2823ac738d01e1f21025ea9e34c444bcdbad31bf
kannibalox <kannibalox@gmail.com>
2019-03-10 21:19:34 +0000

    Brute force it for now

--- setup.py
+++ setup.py
@@ -23,8 +23,13 @@ setup(
     ],
     packages=find_packages('src'),
     package_dir={'':'src'},
-    package_data={'': ['templates/*.html'] +
-                  package_files('src/PtpUploader/WebServer/static/')},
+    package_data={'': ['templates/*.html',
+                       'static/*',
+                       'static/*/*',
+                       'static/*/*/*',
+                       'static/*/*/*/*'
+    ]
+    },
     license='MIT',
     entry_points={
         'console_scripts': [
