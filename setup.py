dd3220453f893d408a95326bb6e2ffade379049f
kannibalox <kannibalox@gmail.com>
2019-03-10 21:12:14 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import os
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
 
 setup(
     name="PtpUploader",
@@ -16,7 +24,7 @@ setup(
     packages=find_packages('src'),
     package_dir={'':'src'},
     package_data={'': ['templates/*.html',
-                       'static/*', 'static/script/*', 'static/source_icon/*']},
+                       'src/PtpUploader/WebServer/static/']},
     license='MIT',
     entry_points={
         'console_scripts': [
