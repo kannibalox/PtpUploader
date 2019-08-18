73619d2074ffcb768b703d8b3d3eb4104da22910
sunn0 <sunn0@riseup.net>
2019-08-18 08:32:56 +0000

    Update Imges.py
    readd necessary imports

--- src/PtpUploader/ImageHost/Imges.py
+++ src/PtpUploader/ImageHost/Imges.py
@@ -5,6 +5,8 @@ import os
 import urllib
 import urllib2
 import uuid
+import poster
+import simplejson as json
 
 class Imges:
         RequiredHttpHeader = { "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:18.0) Gecko/20100101 Firefox/18.0" }
