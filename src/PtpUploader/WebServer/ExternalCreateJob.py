43d700b691be9cbca124af490b084022818f4eba
bobbintb <bobbintb@gmail.com>
2020-02-28 18:14:07 -0700

    Update ExternalCreateJob.py

--- src/PtpUploader/WebServer/ExternalCreateJob.py
+++ src/PtpUploader/WebServer/ExternalCreateJob.py
@@ -11,7 +11,7 @@ from ..ReleaseInfo import ReleaseInfo
 from ..Settings import Settings
 
 from flask import jsonify, make_response, request
-from werkzeug import secure_filename
+from werkzeug.utils import secure_filename
 
 import os
 import sys
