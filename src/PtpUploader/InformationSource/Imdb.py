4ce0e66628478947aebce295c630c3f358464fbe
kannibalox <kannibalox@gmail.com>
2021-10-23 22:15:25 +0000

    Simplify imdb code

--- src/PtpUploader/InformationSource/Imdb.py
+++ src/PtpUploader/InformationSource/Imdb.py
@@ -1,92 +1,35 @@
-import re
-
-import requests
-from bs4 import BeautifulSoup as bs4
-
+from imdb import IMDb
 
 class ImdbInfo:
     def __init__(self):
-        self.Title = ""
-        self.Year = ""
-        self.PosterUrl = ""
-        self.Plot = ""
-        self.IsSeries = False
-        self.ImdbRating = ""
-        self.ImdbVoteCount = ""
+        self.Title: str = ""
+        self.Year: str = ""
+        self.PosterUrl: str = ""
+        self.Plot: str = ""
+        self.IsSeries: bool = False
+        self.ImdbRating: str = ""
+        self.ImdbVoteCount: str = ""
 
 
 class Imdb:
     @staticmethod
-    def __GetInfoInternal(imdbId):
-        imdbInfo = ImdbInfo()
-        soup = bs4(
-            requests.get("https://www.imdb.com/title/tt{}/".format(imdbId)).text,
-            "html.parser",
-        )
-
-        if (
-            "TV Episode" in soup.find("title").text
-            or "TV Series" in soup.find("title").text
-        ):
-            imdbInfo.IsSeries = True
-        else:
-            imdbInfo.IsSeries = False
-
-        if imdbInfo.IsSeries:
-            title_str = soup.find(class_="title_wrapper").find("h1").text
-            match = re.search(r"(.*) \((\d+)\)", title_str)
-            imdbInfo.Title = match.group(1).strip()
-            imdbInfo.Year = match.group(2)
-        else:
-            imdbInfo.Title = (
-                soup.find(class_="title_wrapper").find("h1").find(text=True).strip()
-            )
-            imdbInfo.Year = (
-                soup.find(class_="title_wrapper")
-                .find("h1")
-                .find("span")
-                .text.strip("()")
-            )
-
-        rating = soup.find(itemprop="ratingValue")
-        if rating is not None:
-            imdbInfo.ImdbRating = soup.find(itemprop="ratingValue").text
-            imdbInfo.ImdbVoteCount = soup.find(itemprop="ratingCount").text.replace(
-                ",", ""
-            )
-        else:
-            imdbInfo.ImdbRating = "0.0"
-            imdbInfo.ImdbVoteCount = "0"
-
-        poster = soup.find(class_="poster")
-        if poster is not None:
-            imdbInfo.PosterUrl = re.sub(
-                r"\._V1_.*\.jpg", "._V1_SY768_.jpg", poster.find("img")["src"]
-            )
-
-        imdbInfo.Plot = ""
-        if soup.find(id="titleStoryLine") is not None:
-            imdbInfo.Plot = soup.find(id="titleStoryLine").find("div").text.strip()
-        else:
-            plot_soup = bs4(
-                requests(
-                    "https://www.imdb.com/title/tt{}/plotsummary".format(imdbId)
-                ).text
-            )
-            imdbInfo.Plot = plot_soup.find(id="plot-summaries-content").find("li").text
-        return imdbInfo
-
-    @staticmethod
-    def GetInfo(logger, imdbId):
+    def GetInfo(logger, imdbId: str) -> ImdbInfo:
         logger.info("Getting IMDb info for IMDb id '%s'." % imdbId)
 
         # We don't care if this fails. It gives only extra information.
+        imdbInfo = ImdbInfo()
         try:
-            imdbInfo = Imdb.__GetInfoInternal(imdbId)
-            return imdbInfo
+            ia = IMDb()
+            movie = ia.get_movie(imdbId.strip('t'))
+            imdbInfo.Title = movie['title']
+            imdbInfo.Year = str(movie['year'])
+            imdbInfo.ImdbRating = movie['rating']
+            imdbInfo.ImdbVoteCount = movie['votes']
+            imdbInfo.PosterUrl = movie['full-size cover url']
+            imdbInfo.Plot = movie['plot'][0]
         except Exception:
             logger.exception(
                 "Got exception while trying to get IMDb info by IMDb id '%s'." % imdbId
             )
 
-        return ImdbInfo()
+        return imdbInfo
