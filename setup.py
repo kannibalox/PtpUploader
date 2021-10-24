6c5c32ff56354d0922b9e9592844f6ed19f2a336
kannibalox <kannibalox@gmail.com>
2021-10-24 04:41:02 +0000

    Formatting

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
 import os
+
+from setuptools import setup, find_packages
+
 
 def package_files(directory):
     paths = []
-    for (path, directories, filenames) in os.walk(directory):
+    for (path, _, filenames) in os.walk(directory):
         for filename in filenames:
-            paths.append(os.path.join('..', path, filename))
+            paths.append(os.path.join("..", path, filename))
     return paths
+
 
 setup(
     name="PtpUploader",
     version="0.1",
     author="kannibalox",
-    url="https://github.com/kannibalox/PTPAPI",
+    url="https://github.com/kannibalox/PtpUploader",
     install_requires=[
         "pyrobase @ git+https://github.com/kannibalox/pyrobase@py3",
         "pyrocore @ git+https://github.com/kannibalox/pyrocore@py3",
@@ -24,20 +27,22 @@ setup(
         "watchdog",
         "beautifulsoup4",
     ],
-    packages=find_packages('src'),
-    package_dir={'':'src'},
-    package_data={'': ['templates/*.html',
-                       'static/*',
-                       'static/*/*',
-                       'static/*/*/*',
-                       'static/*/*/*/*'
-    ]
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    package_data={
+        "": [
+            "templates/*.html",
+            "static/*",
+            "static/*/*",
+            "static/*/*/*",
+            "static/*/*/*/*",
+        ]
     },
-    license='MIT',
+    license="MIT",
     entry_points={
-        'console_scripts': [
-            'PtpUploader=PtpUploader.Main:Main',
-            'ReleaseInfoMaker=PtpUploader.ReleaseInfoMaker:Main'
+        "console_scripts": [
+            "PtpUploader=PtpUploader.Main:Main",
+            "ReleaseInfoMaker=PtpUploader.ReleaseInfoMaker:Main",
         ],
-    }
+    },
 )
