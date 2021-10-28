cf28933737a50cff77ce1f1cb989c8ba089abe1d
kannibalox <kannibalox@gmail.com>
2021-10-28 12:57:30 +0000

    Fix edit route and add fake getlatest

--- src/PtpUploader/web/urls.py
+++ src/PtpUploader/web/urls.py
@@ -7,11 +7,12 @@ urlpatterns = [
     path("", lambda r: redirect("/jobs"), name="index"),
     path("jobs", views.jobs, name="jobs"),
     path("ajax/jobs", views.jobs_json, name="jobs_json"),
+    path("ajax/getlatest", views.jobs_json, name="jobs_get_latest"),
     path("upload", views.jobs, name="upload"),
     path("movieAvailabilityCheck", views.jobs, name="movieAvailabilityCheck"),
     path("quit", views.jobs, name="quit"),
     path("job/<int:r_id>/log", views.log, name="log"),
     path("job/<int:r_id>/stop", views.stop_job, name="stop_job"),
     path("job/<int:r_id>/start", views.start_job, name="start_job"),
-    path("job/<int:r_id>/edit", views.stop_job, name="edit_job"),
+    path("job/<int:r_id>/edit", views.edit_job, name="edit_job"),
 ]
