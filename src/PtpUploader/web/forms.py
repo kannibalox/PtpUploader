24d03dd260a71a433b204e8951faeb7f7e3146f0
kannibalox <kannibalox@gmail.com>
2021-10-28 22:46:52 +0000

    More form conversions

--- src/PtpUploader/web/forms.py
+++ src/PtpUploader/web/forms.py
@@ -2,14 +2,20 @@ from django.forms import ModelForm, ChoiceField, Select, CharField, TextInput
 
 from PtpUploader.ReleaseInfo import ReleaseInfo
 
+
 class ReleaseForm(ModelForm):
-    Type = ChoiceField(choices=[('f', 'f'), ('b', 'b')])
-    ReleaseName = CharField(max_length=60, widget=TextInput(attrs={'size':'60'}))
-    
+    Type = ChoiceField(choices=ReleaseInfo.MediaType.choices)
+    Codec = ChoiceField(choices=ReleaseInfo.CodecChoices.choices)
+    ReleaseName = CharField(max_length=60, widget=TextInput(attrs={"size": "60"}))
+    ImdbId = CharField(widget=TextInput(attrs={"size": "60"}))
+    CoverArtUrl = CharField(widget=TextInput(attrs={"size": "60"}))
+    Directors = CharField(widget=TextInput(attrs={"size": "60"}))
+    Title = CharField(widget=TextInput(attrs={"size": "60"}))
+    Year = CharField(widget=TextInput(attrs={"size": "5"}))
+    RemasterTitle = CharField(widget=TextInput(attrs={"size": "60"}))
+    RemasterYear = CharField(widget=TextInput(attrs={"size": "5"}))
+
     class Meta:
         model = ReleaseInfo
-        fields = ('Container', 'Type', 'ReleaseName', 'Codec', 'Source')
-        labels = {
-            'ReleaseName': 'Release Name'
-        }
-        
+        fields = "__all__"
+        labels = {"ReleaseName": "Release Name"}
