53faba15e2ad7436ca56dc599fe0442ea93159cb
kannibalox <kannibalox@gmail.com>
2021-10-29 23:58:22 +0000

    Load our app last

--- src/PtpUploader/web/settings.py
+++ src/PtpUploader/web/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = "*"
 # Application definition
 
 INSTALLED_APPS = [
-    "PtpUploader.web",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "PtpUploader.web",
 ]
 
 MIDDLEWARE = [
