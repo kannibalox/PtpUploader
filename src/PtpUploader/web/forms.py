1a30596ef6b83157482f5a4bbc2700ef83ecf3c1
kannibalox <kannibalox@gmail.com>
2021-10-29 00:32:09 +0000

    Migrate choices to Form

--- src/PtpUploader/web/forms.py
+++ src/PtpUploader/web/forms.py
@@ -1,12 +1,17 @@
-from django.forms import ModelForm, ChoiceField, Select, CharField, TextInput
+from django.forms import ModelForm, ChoiceField, Select, CharField, TextInput, MultipleChoiceField
 
 from PtpUploader.ReleaseInfo import ReleaseInfo
+from PtpUploader.PtpSubtitle import PtpSubtitleId
 
 
 class ReleaseForm(ModelForm):
     Type = ChoiceField(choices=ReleaseInfo.MediaType.choices)
-    Codec = ChoiceField(choices=ReleaseInfo.CodecChoices.choices)
+    Codec = ChoiceField(choices=[("---", "---")] + ReleaseInfo.CodecChoices.choices)
+    Container = ChoiceField(choices=[("---", "---")] + ReleaseInfo.ContainerChoices.choices)
+    Source = ChoiceField(choices=[("---", "---")] + ReleaseInfo.SourceChoices.choices)
+    ResolutionType = ChoiceField(choices=[("---", "---")] + ReleaseInfo.ResolutionChoices.choices)
     ReleaseName = CharField(max_length=60, widget=TextInput(attrs={"size": "60"}))
+    Subtitles = MultipleChoiceField(choices=[(v,k) for k, v in PtpSubtitleId.__dict__.items() if not k.startswith('_')])
     ImdbId = CharField(widget=TextInput(attrs={"size": "60"}))
     CoverArtUrl = CharField(widget=TextInput(attrs={"size": "60"}))
     Directors = CharField(widget=TextInput(attrs={"size": "60"}))
