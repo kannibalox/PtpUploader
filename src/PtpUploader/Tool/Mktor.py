b55ff4e65a0bdf855bfe0c76bdeef070dfdfd89a
kannibalox <kannibalox@gmail.com>
2022-01-25 16:27:48 +0000

    Re-use existing .torrents if still valid

--- src/PtpUploader/Tool/Mktor.py
+++ src/PtpUploader/Tool/Mktor.py
@@ -1,28 +1,30 @@
 import os
 
+import bencode
 from pyrosimple.util import metafile
 
-from PtpUploader.PtpUploaderException import PtpUploaderException
 from PtpUploader.Settings import Settings
 
 
 def Make(logger, path, torrentPath):
-    def Callback(meta):
+    def callback(meta):
         meta["info"]["source"] = "PTP"
 
     logger.info("Making torrent from '%s' to '%s'." % (path, torrentPath))
     torrent = metafile.Metafile(torrentPath, datapath=path)
 
     if os.path.exists(torrentPath):
-        raise PtpUploaderException(
-            "Can't create torrent because path '%s' already exists." % torrentPath
+        with open(torrentPath, "rb") as fh:
+            # Ignore the result of this method, we just want to check that files haven't changed/moved
+            metafile.add_fast_resume(bencode.decode(fh.read()), path)
+            logger.info("Using existing torrent file at '%s'.", torrentPath)
+    else:
+        logger.info("Making torrent from '%s' to '%s'.", path, torrentPath)
+        torrent.create(
+            path,
+            Settings.PtpAnnounceUrl,
+            created_by="PtpUploader",
+            private=True,
+            progress=None,
+            callback=callback,
         )
-
-    torrent.create(
-        path,
-        Settings.PtpAnnounceUrl,
-        created_by="PtpUploader",
-        private=True,
-        progress=None,
-        callback=Callback,
-    )
