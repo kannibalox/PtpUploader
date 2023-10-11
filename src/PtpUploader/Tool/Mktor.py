d19577d8774463a2d5af6875433425bf2864c2d9
kannibalox <kannibalox@gmail.com>
2023-10-11 16:08:35 -0400

    Use file_generator for torrent whitelist

--- src/PtpUploader/Tool/Mktor.py
+++ src/PtpUploader/Tool/Mktor.py
@@ -14,16 +14,10 @@ def Make(logger, path, torrentPath, includedFileList: Optional[List[str]] = None
 
     ignore = []
     path = Path(path)
-    print(includedFileList)
-    if includedFileList:
-        for subpath in path.rglob("*"):
-            if not subpath.is_file():
-                continue
-            subpath = subpath.relative_to(path)
-            print(subpath)
-            if subpath not in includedFileList:
-                print(str(subpath))
-                ignore.append(re.compile('^'+str(subpath)+'$'))
+    def ptpup_walk(datapath: Path):
+        for subpath in datapath.rglob("*"):
+            if subpath.is_file() and str(subpath.relative_to(path)) in includedFileList:
+                yield subpath
     if os.path.exists(torrentPath):
         # We should be safe to allow the existing torrent to be used,
         # even when/if file selection is re-implemented, all the filesystem
@@ -37,14 +31,13 @@ def Make(logger, path, torrentPath, includedFileList: Optional[List[str]] = None
         logger.info("Using existing torrent file at '%s'.", torrentPath)
     else:
         logger.info("Making torrent from '%s' to '%s'.", path, torrentPath)
-        print(ignore)
         metafile = Metafile.from_path(
             Path(path),
             config.ptp.announce_url,
             created_by="PtpUploader",
             private=True,
             progress=None,
-            whitelist=includedFileList,
+            file_generator=ptpup_walk
         )
         metafile["info"]["source"] = "PTP"
         metafile.save(Path(torrentPath))
